29cdc15d9401d23431b0e8ee202a01b4b2d57439
“ST <shaotian.li.april@gmail.com>
2025-06-04 12:33:40 +0800

    move locale dir to src so that translation can be switched without http request

--- src/i18n.js
+++ src/i18n.js
@@ -1,18 +1,25 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
+// 直接导入翻译文件
+import enTranslation from './locales/en/translation.json';
+import zhTranslation from './locales/zh/translation.json';
+
 i18n
-  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    resources: {
+      en: {
+        translation: enTranslation
+      },
+      zh: {
+        translation: zhTranslation
+      }
+    },
     fallbackLng: 'en',
     debug: process.env.NODE_ENV === 'development',
-    backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json',
-    },
     interpolation: {
       escapeValue: false
     }
